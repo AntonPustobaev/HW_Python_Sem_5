ce2cd68d8c2f1d4a54ae1965d2d082676c73a24c
Antonio <don_ne@mail.ru>
2022-07-24 20:02:43 +0400

    Task2.1

--- Task_2.py
+++ Task_2.py
@@ -1,12 +1,13 @@
+# Дан список чисел. Создайте список, в который попадают числа, описываемые возрастающую последовательность. 
+# Порядок элементов менять нельзя.
 
-def Get_growing_row(list):
-    growing = [list[0]]
+def get_up_num(list):
+    ups = [list[0]]
     for i in list:
-        if growing[-1] < i:
-            growing.append(i)
-    return growing
-
+        if i > max(ups):
+            ups.append(i)
+    return ups    
 
 list = [1, 5, 2, 3, 6, 8, 3, 4, 1, 7, 9]
 print(list)
-print(Get_growing_row(list))
+print(get_up_num(list))
